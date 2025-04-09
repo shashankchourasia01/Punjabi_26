f78e2735ddd1214fcb1d06a5cfb20db4073851e9
shashank chourasia01 <shashankchourasia0829@gmail.com>
2025-04-09 16:52:34 +0530

    fix weather api

--- src/components/More.jsx
+++ src/components/More.jsx
@@ -31,11 +31,24 @@ const More = () => {
     <div >
         <h1 className=" flex items-center justify-center font-semibold text-4xl">All Categories</h1>
         {/* Close Button (Top-Right) */}
-      <button 
-        onClick={() => navigate(-1)} // Navigate to previous page
-        className="absolute  right-4 top-1/2 transform -translate-y-1/2 text-gray-600 hover:text-red-600 text-3xl"
+        <button
+        onClick={() => window.history.back()}
+        className="absolute top-4 right-4 text-black cursor-pointer"
       >
-        <IoClose />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
       </button>
       <div
         id="explore"
