23fba8f6c01238fbd2a1aa184dccda380e6846b4
shashank chourasia01 <shashankchourasia0829@gmail.com>
2025-08-26 21:09:22 +0530

    qffff

--- src/services/Helper.jsx
+++ src/services/Helper.jsx
@@ -1,3 +1,3 @@
-// export const SERVER_URL = "http://localhost:5000/api/"
+//export const SERVER_URL = "http://localhost:5000/api/"
 // export const SERVER_URL = "https://t5bj8gmz-5000.inc1.devtunnels.ms/api/"
 export const SERVER_URL = "https://punjabi-pages-test-backend.onrender.com"
